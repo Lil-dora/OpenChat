46a350d6c3ffec88fdb9a43db3a4a4841c488019
Lil Dora <lildora0101@gmail.com>
2021-12-27 22:32:13 +0600

    yes

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const Main = () =>{
+  return(
+  <>
+    <App/>
+  </>)
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Main />,
   document.getElementById('root')
 );
 
